57609861ea69ea653c2de1b6c536868c01c8c49b
Ojas Patwardhan <ojaspatwardhan@Ojass-MacBook-Pro.local>
2019-12-18 19:02:09 +0530

    backend first commit

---
+++ mywebsite/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Project, Hobby
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('id', 'title', 'description', 'link')
+
+class HobbySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hobby
+        fields = ('id', 'title', 'description')
