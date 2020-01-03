e79dc48d377bdef2d961ae5e60f0dac02e78ff2b
ojaspatwardhan <patwardhan.o@husky.neu.edu>
2020-01-03 22:03:43 +0530

    Updated database url

--- backend/settings.py
+++ backend/settings.py
@@ -79,17 +79,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
+# Database updates for Heroku
+prod_db = dj_database_url.config()
+DATABASES['default'].update(prod_db)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -154,7 +153,3 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-# Database updates for Heroku
-# prod_db = dj_database_url.config()
-# DATABASES['default'].update(prod_db)
